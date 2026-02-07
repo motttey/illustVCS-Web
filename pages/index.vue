ef2de8b9d1fd4d9fb388c746e64cc052579188c0
motttey <motitago@gmail.com>
2026-02-08 00:03:32 +0900

    fix: font sizes

--- pages/index.vue
+++ pages/index.vue
@@ -724,16 +724,15 @@ onBeforeUnmount(() => {
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
+  font-weight: 500;
+  font-size: 72px;
   color: #35495e;
-  letter-spacing: 1px;
+  letter-spacing: 10px;
 }
 
 .subtitle {
   font-weight: 300;
-  font-size: 42px;
+  font-size: 36px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
